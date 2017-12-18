e82760b102496198f174b10d87b55a176b733a2e
JinGo1020 <1029605520@qq.com>
2017-12-18 12:03:54 +0800

    jzy llz

--- counter.js
+++ counter.js
@@ -3,6 +3,8 @@ var c=0
 var t
 function timedCount()
 {
+document.getElementById("start").style.display="none";
+document.getElementById("stop").style.display="inline";
 document.getElementById('txt').value=c
 c=c+1
 t=setTimeout("timedCount()",1000)
@@ -10,6 +12,8 @@ t=setTimeout("timedCount()",1000)
 
 function stopCount()
 {
+document.getElementById("start").style.display="inline";
+document.getElementById("stop").style.display="none";
 c=0;
 setTimeout("document.getElementById('txt').value=0",0);
 clearTimeout(t);
